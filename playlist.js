4d7a6e43d3d179e4f815ab26204f321ad275b48e
Michael Matiychenko <michaelmatiychenko@gmail.com>
2026-07-29 21:00:12 -0400

    t

--- playlist.js
+++ playlist.js
@@ -12,7 +12,7 @@ function playNext() {
     if (!playing) return;
     const randomIndex = Math.floor(Math.random() * audioSources.length);
     currentAudio.src = audioSources[randomIndex];
-    currentAudio.defaultPlaybackRate = 0.07;\
+    currentAudio.defaultPlaybackRate = 0.07;
     currentAudio.playbackRate = 0.07;
     currentAudio.play();
     currentAudio.onended = () => {
